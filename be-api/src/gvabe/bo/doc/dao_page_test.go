f6f2989bcc63cee242ad1c045bcbc41db05fdf1e
Thanh Nguyen <btnguyen2k@gmail.com>
2021-09-04 14:18:38 +1000

    update tests

--- be-api/src/gvabe/bo/doc/dao_page_test.go
+++ be-api/src/gvabe/bo/doc/dao_page_test.go
@@ -2,6 +2,7 @@ package doc
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"testing"
 	"time"
@@ -132,137 +133,152 @@ func doTestPageDaoCreateGet(t *testing.T, name string, dao PageDao) {
 }
 
 func doTestPageDaoCreateUpdateGet(t *testing.T, name string, dao PageDao) {
-	// _tagVersion := uint64(1337)
-	// _appId := "libro"
-	// _name := "Libro"
-	// _desc := "Libro description"
-	// _isVisible := true
-	// _app := app.NewApp(_tagVersion, _appId, _name, _desc, _isVisible)
-	//
-	// _title := "Quick start"
-	// _icon := "default"
-	// _summary := "page one"
-	// _pos := rand.Intn(10242048)
-	// bo0 := NewPage(_tagVersion, _app, _title, _icon, _summary)
-	// bo0.SetPosition(_pos)
-	// _email := "libro@libro"
-	// _age := float64(35)
-	// bo0.SetDataAttr("props.owner", _name)
-	// bo0.SetDataAttr("props.email", _email)
-	// bo0.SetDataAttr("age", _age)
-	// if ok, err := dao.Create(bo0); err != nil || !ok {
-	// 	t.Fatalf("%s failed: %#v / %s", name+"/Create", ok, err)
-	// }
-	//
-	// bo0.SetAppId(_appId + "-new").SetIcon(_icon + "-new").SetTitle(_title + "-new").SetSummary(_summary + "-new").SetPosition(_pos + 1).SetTagVersion(_tagVersion + 3)
-	// bo0.SetDataAttr("props.owner", _name+"-new")
-	// bo0.SetDataAttr("props.email", _email+"-new")
-	// bo0.SetDataAttr("age", _age+2)
-	// if ok, err := dao.Update(bo0); err != nil {
-	// 	t.Fatalf("%s failed: %s", name+"/Update", err)
-	// } else if !ok {
-	// 	t.Fatalf("%s failed: cannot update record", name)
-	// }
-	//
-	// _id := bo0.GetId()
-	// if bo1, err := dao.Get(_id); err != nil || bo1 == nil {
-	// 	t.Fatalf("%s failed: nil or error %s", name+"/Get("+_id+")", err)
-	// } else {
-	// 	if v1, v0 := bo1.GetDataAttrAsUnsafe("props.owner", reddo.TypeString), _name+"-new"; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetDataAttrAsUnsafe("props.email", reddo.TypeString), _email+"-new"; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetDataAttrAsUnsafe("age", reddo.TypeInt), int64(_age+2); v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetTagVersion(), _tagVersion+3; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetId(), _id; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetAppId(), _app.GetId()+"-new"; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetIcon(), _icon+"-new"; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetTitle(), _title+"-new"; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetSummary(), _summary+"-new"; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if v1, v0 := bo1.GetPosition(), _pos+1; v1 != v0 {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
-	// 	}
-	// 	if t1, t0 := bo1.GetTimeCreated(), bo0.GetTimeCreated(); !t1.Equal(t0) {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, t0.Format(time.RFC3339), t1.Format(time.RFC3339))
-	// 	}
-	// 	if bo1.GetChecksum() != bo0.GetChecksum() {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v", name, bo0.GetChecksum(), bo1.GetChecksum())
-	// 	}
-	// }
+	_tagVersion := uint64(1337)
+	_appId := "libro"
+	_name := "Libro"
+	_desc := "Libro description"
+	_isVisible := true
+	_app := app.NewApp(_tagVersion, _appId, _name, _desc, _isVisible)
+
+	_title := "Quick start"
+	_icon := "default"
+	_summary := "topic one"
+	_pos := rand.Intn(10242048)
+	_topic := NewTopic(_tagVersion, _app, _title, _icon, _summary)
+	_topic.SetPosition(_pos)
+
+	_content := "page one"
+	bo0 := NewPage(_tagVersion, _topic, _title+"-page", _icon+"-page", _summary+"-page", _content)
+	bo0.SetPosition(_pos + 1)
+	_email := "libro@libro"
+	_age := float64(35)
+	bo0.SetDataAttr("props.owner", _name)
+	bo0.SetDataAttr("props.email", _email)
+	bo0.SetDataAttr("age", _age)
+	if ok, err := dao.Create(bo0); err != nil || !ok {
+		t.Fatalf("%s failed: %#v / %s", name+"/Create", ok, err)
+	}
+
+	bo0.SetAppId(_appId + "-new").SetTopicId(_topic.GetId() + "-new").
+		SetIcon(_icon + "-new").SetTitle(_title + "-new").SetSummary(_summary + "-new").SetPosition(_pos + 2).SetContent(_content + "-new").
+		SetTagVersion(_tagVersion + 3)
+	bo0.SetDataAttr("props.owner", _name+"-new")
+	bo0.SetDataAttr("props.email", _email+"-new")
+	bo0.SetDataAttr("age", _age+2)
+	if ok, err := dao.Update(bo0); err != nil {
+		t.Fatalf("%s failed: %s", name+"/Update", err)
+	} else if !ok {
+		t.Fatalf("%s failed: cannot update record", name)
+	}
+
+	_id := bo0.GetId()
+	if bo1, err := dao.Get(_id); err != nil || bo1 == nil {
+		t.Fatalf("%s failed: nil or error %s", name+"/Get("+_id+")", err)
+	} else {
+		if v1, v0 := bo1.GetDataAttrAsUnsafe("props.owner", reddo.TypeString), _name+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetDataAttrAsUnsafe("props.email", reddo.TypeString), _email+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetDataAttrAsUnsafe("age", reddo.TypeInt), int64(_age+2); v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetTagVersion(), _tagVersion+3; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetId(), _id; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetAppId(), _app.GetId()+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetTopicId(), _topic.GetId()+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetIcon(), _icon+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetTitle(), _title+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetSummary(), _summary+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetPosition(), _pos+2; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if v1, v0 := bo1.GetContent(), _content+"-new"; v1 != v0 {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, v0, v1)
+		}
+		if t1, t0 := bo1.GetTimeCreated(), bo0.GetTimeCreated(); !t1.Equal(t0) {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, t0.Format(time.RFC3339), t1.Format(time.RFC3339))
+		}
+		if bo1.GetChecksum() != bo0.GetChecksum() {
+			t.Fatalf("%s failed: expected %#v but received %#v", name, bo0.GetChecksum(), bo1.GetChecksum())
+		}
+	}
 }
 
 func doTestPageDaoCreateDelete(t *testing.T, name string, dao PageDao) {
-	// _tagVersion := uint64(1337)
-	// _appId := "libro"
-	// _name := "Libro"
-	// _desc := "Libro description"
-	// _isVisible := true
-	// _app := app.NewApp(_tagVersion, _appId, _name, _desc, _isVisible)
-	//
-	// _title := "Quick start"
-	// _icon := "default"
-	// _summary := "page one"
-	// _pos := rand.Intn(10242048)
-	// bo0 := NewPage(_tagVersion, _app, _title, _icon, _summary)
-	// bo0.SetPosition(_pos)
-	// _email := "libro@libro"
-	// _age := float64(35)
-	// bo0.SetDataAttr("props.owner", _name)
-	// bo0.SetDataAttr("props.email", _email)
-	// bo0.SetDataAttr("age", _age)
-	// if ok, err := dao.Create(bo0); err != nil || !ok {
-	// 	t.Fatalf("%s failed: %#v / %s", name+"/Create", ok, err)
-	// }
-	//
-	// _id := bo0.GetId()
-	// if bo1, err := dao.Get(_id); err != nil || bo1 == nil {
-	// 	t.Fatalf("%s failed: nil or error %s", name+"/Get("+_id+")", err)
-	// } else if ok, err := dao.Delete(bo1); !ok || err != nil {
-	// 	t.Fatalf("%s failed: not-ok or error %s", name+"/Delete("+_id+")", err)
-	// }
-	// if bo1, err := dao.Get(_id); err != nil || bo1 != nil {
-	// 	t.Fatalf("%s failed: not-nil or error %s", name+"/Get("+_id+")", err)
-	// }
+	_tagVersion := uint64(1337)
+	_appId := "libro"
+	_name := "Libro"
+	_desc := "Libro description"
+	_isVisible := true
+	_app := app.NewApp(_tagVersion, _appId, _name, _desc, _isVisible)
+
+	_title := "Quick start"
+	_icon := "default"
+	_summary := "topic one"
+	_pos := rand.Intn(10242048)
+	_topic := NewTopic(_tagVersion, _app, _title, _icon, _summary)
+	_topic.SetPosition(_pos)
+
+	_content := "page one"
+	bo0 := NewPage(_tagVersion, _topic, _title+"-page", _icon+"-page", _summary+"-page", _content)
+	bo0.SetPosition(_pos + 1)
+	_email := "libro@libro"
+	_age := float64(35)
+	bo0.SetDataAttr("props.owner", _name)
+	bo0.SetDataAttr("props.email", _email)
+	bo0.SetDataAttr("age", _age)
+	if ok, err := dao.Create(bo0); err != nil || !ok {
+		t.Fatalf("%s failed: %#v / %s", name+"/Create", ok, err)
+	}
+
+	_id := bo0.GetId()
+	if bo1, err := dao.Get(_id); err != nil || bo1 == nil {
+		t.Fatalf("%s failed: nil or error %s", name+"/Get("+_id+")", err)
+	} else if ok, err := dao.Delete(bo1); !ok || err != nil {
+		t.Fatalf("%s failed: not-ok or error %s", name+"/Delete("+_id+")", err)
+	}
+	if bo1, err := dao.Get(_id); err != nil || bo1 != nil {
+		t.Fatalf("%s failed: not-nil or error %s", name+"/Get("+_id+")", err)
+	}
 }
 
 func doTestPageDaoGetAll(t *testing.T, name string, dao PageDao) {
-	// initSampleRowsPage(t, name, dao)
-	// for _, app := range appList {
-	// 	boList, err := dao.GetAll(app, nil, nil)
-	// 	expected := appPageCount[app.GetId()]
-	// 	if err != nil || len(boList) != expected {
-	// 		t.Fatalf("%s failed: expected %#v but received %#v (error %s)", name+"/GetAll", expected, len(boList), err)
-	// 	}
-	// }
+	initSampleRowsPage(t, name, dao)
+	for _, topic := range topicList {
+		boList, err := dao.GetAll(topic, nil, nil)
+		expected := topicPageCount[topic.GetId()]
+		if err != nil || len(boList) != expected {
+			t.Fatalf("%s failed: expected %#v but received %#v (error %s)", name+"/GetAll", expected, len(boList), err)
+		}
+	}
 }
 
 func doTestPageDaoGetN(t *testing.T, name string, dao PageDao) {
-	// initSampleRowsPage(t, name, dao)
-	// for _, app := range appList {
-	// 	startOffset := rand.Intn(5)
-	// 	numRowsLimit := rand.Intn(10) + 1
-	// 	boList, err := dao.GetN(app, startOffset, numRowsLimit, nil, nil)
-	// 	expected := appPageCount[app.GetId()]
-	// 	expected = int(math.Min(math.Max(0, float64(expected-startOffset)), float64(numRowsLimit)))
-	// 	if err != nil || len(boList) != expected {
-	// 		// fmt.Printf("%s - %#v - %#v / %#v\n", app.GetId(), startOffset, numRowsLimit, appPageCount)
-	// 		t.Fatalf("%s failed: expected %#v but received %#v (error %s)", name+"/"+app.GetId(), expected, len(boList), err)
-	// 	}
-	// }
+	initSampleRowsPage(t, name, dao)
+	for _, topic := range topicList {
+		startOffset := rand.Intn(5)
+		numRowsLimit := rand.Intn(10) + 1
+		boList, err := dao.GetN(topic, startOffset, numRowsLimit, nil, nil)
+		expected := topicPageCount[topic.GetId()]
+		expected = int(math.Min(math.Max(0, float64(expected-startOffset)), float64(numRowsLimit)))
+		if err != nil || len(boList) != expected {
+			t.Fatalf("%s failed: expected %#v but received %#v (error %s)", name+"/"+topic.GetId(), expected, len(boList), err)
+		}
+	}
 }
