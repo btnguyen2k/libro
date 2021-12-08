989e00d84737da33ae775ce8a0ca65a647d4e465
Thanh Nguyen <thanhn@microsoft.com>
2021-12-08 16:07:33 +1100

    add page footer displaying Libro info

--- fe-tpl-coderdocs/src/components/Topic.vue
+++ fe-tpl-coderdocs/src/components/Topic.vue
@@ -146,8 +146,9 @@
     <footer class="footer">
       <div class="footer-bottom text-center py-5">
         <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-        <small class="copyright">Designed with <ficon style="color: #fb866a;" :icon="['fas', 'heart']"/>
-          by <a class="theme-link" href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers</small>
+        <small class="copyright">Designed with <ficon style="color: #fb866a;" :icon="['fas', 'heart']"/> by <a class="theme-link" href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers.</small>
+        <br/>
+        <small class="copyright">Site built with <a class="theme-link" href="https://github.com/btnguyen2k/libro" target="_blank">Libro</a>.</small>
       </div>
     </footer>
   </div>
